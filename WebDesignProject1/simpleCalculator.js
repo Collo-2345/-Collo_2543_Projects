98ef22a09700f58703d680c2664ae7146dcf738a
Collo-2345 <kololamoses@gmail.com>
2025-02-21 12:20:16 +0300

    Add files via upload

---
+++ WebDesignProject1/simpleCalculator.js
@@ -0,0 +1,21 @@
+// CALCULATOR FUNCTION
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+     display.value += input;
+}
+
+function ClearScreen(){
+    display.value ="";
+}
+ 
+function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "ERROR"; 
+    }
+    
+}
